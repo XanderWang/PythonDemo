5fa5983432e892227a27986f7432bf5c05b5be3a
汪小阳 <420640763@qq.com>
2016-03-11 22:55:38 +0800

    add the list tuple demo

--- Project.py
+++ Project.py
@@ -6,6 +6,10 @@ import os
 TYPE_NORMAL = 0;
 TYPE_STUDIO = 1;
 
+class ProjectDir :
+    rootDir = '';
+    type = TYPE_NORMAL;
+
 def analyseType(dirPath) :
     if( os.path.isdir(dirPath) ) :
         childs = os.path.list
